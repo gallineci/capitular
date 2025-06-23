61fb4effca70096f9be6479501fbbffb8b19a06e
gallineci <necigalli@gmail.com>
2025-06-23 17:59:24 -0300

    tentando rodar foto

--- historia/urls.py
+++ historia/urls.py
@@ -4,4 +4,5 @@ from historia.views import *
 urlpatterns = [
     path ('',ListarHistorias.as_view(), name = 'listar-historias'),
     path ('novo/', CriarHistorias.as_view(), name='criar-historias'),
+    path ('fotos/<str:arquivo>/', FotoHistoria.as_view(), name='foto-historia'),
 ]
--- historia/views.py
+++ historia/views.py
@@ -1,6 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
+from django.views.generic import View
+from django.http import FileResponse, Http404
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Historia
 from .forms import FormularioHistoria
@@ -19,3 +22,23 @@ class CriarHistorias(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.autor = self.request.user
         return super().form_valid(form)
+    
+    def post(self, request, *args, **kwargs):
+        print(">>> Chegou no POST")
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        print(">> form_valid foi chamado")
+        print("Foto:", form.cleaned_data.get("foto"))
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+
+class FotoHistoria (View):
+    def get (self, request, arquivo):
+        try:
+            historia = Historia.objects.get (foto='historia/fotos/{}'.format(arquivo))
+            return FileResponse (historia.foto)
+        except ObjectDoesNotExist:
+            raise Http404 ("Foto não encontrada ou acesso não autorizado")
+        except Exception as exception:
+            raise exception 
